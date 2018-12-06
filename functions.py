ad2bc68c72c2f76eba163bd74ee5090198273ca1
Antonio Dudarev <antonio.dudarev@gtt.net>
2018-12-06 20:43:46 +0000

    Checking input params, adding assert testing

--- functions.py
+++ functions.py
@@ -1,22 +1,36 @@
 import re
 
-def split_line_by_delimiter(delimiter="\t"):
-    return list(re.split(r"{}".format(delimiter)))
+def split_line_by_delimiter(line, delimiter="\t"):
+    if len(delimiter) != 1:
+        raise ValueError("the delimiter must be a single character")
+    return list(re.split(r"{}".format(delimiter), str(line)))
 
 def choose_fields(index_field_list, list_of_fields):
+    if min(index_field_list) < 1:
+        raise ValueError("fields are numbered from 1")
     result = []
     for field in index_field_list:
         try:
-            result.append(str(list_of_fields[int(field)]))
+            result.append(str(list_of_fields[int(field-1)]))
         except IndexError:
             result.append("")
     return result
 
 def choose_character(index_character_list, line):
+    if min(index_character_list) < 1:
+        raise ValueError("byte/character positions are numbered from 1")
     result = []
     for index in index_character_list:
         try:
-            result.append(str(line[index]))
+            result.append(str(line[index-1]))
         except IndexError:
             result.append("")
     return result
+
+
+assert split_line_by_delimiter("Hello World", " ") == ["Hello", "World"]
+assert choose_fields([1], split_line_by_delimiter("Hello World", " ")) == ["Hello"]
+assert choose_fields([1,2], split_line_by_delimiter("Hello World", " ")) == ["Hello", "World"]
+
+assert choose_character([1,2,3,4], "Hello World") == ["H", "e", "l", "l"]
+assert choose_character([1], "Hello World") == ["H"]
