338fb6554bb2b06465225011d84d413f834ad7da
Alex Willmer <alex@moreati.org.uk>
2018-12-06 20:49:43 +0000

    Make -b/-c/-f mutually exclusive

--- cli.py
+++ cli.py
@@ -3,17 +3,20 @@ import sys
 
 def parse_args():
     parser = argparse.ArgumentParser(description='remove sections from each line of files')
-    parser.add_argument('--bytes', '-b', metavar='LIST',
     parser.add_argument('files', type=argparse.FileType('r'), default=[sys.stdin],
                         nargs='*'
                         )
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--bytes', '-b', metavar='LIST',
                         help='select only these bytes')
-    parser.add_argument('--characters', '-c', metavar='LIST',
+    group.add_argument('--characters', '-c', metavar='LIST',
                         help='select only these characters')
+    group.add_argument('--fields', '-f', metavar='LIST',
+                       help='select  only  these  fields;  also print any line that contains no delimiter character, unless the -s option is specified')
+
     parser.add_argument('--delimiter', '-d', metavar='DELIM',
                         help='use DELIM instead of TAB for field delimiter')
-    parser.add_argument('--fields', '-f', metavar='LIST',
-                        help='select  only  these  fields;  also print any line that contains no delimiter character, unless the -s option is specified')
+
     parser.add_argument('-n',
                         help='(ignored)')
     parser.add_argument('--complement', 
